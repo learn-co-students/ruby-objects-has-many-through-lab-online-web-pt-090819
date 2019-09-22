8b2403ef03e2d5544f5af91e7a0be73447b5c089
Darrel Castellano <dcastellano2007@gmail.com>
2019-09-21 17:10:31 -0700

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,15 @@
+class Song
+  attr_accessor :artist, :genre, :title
+  @@all = []
+
+  def initialize(title, artist, genre)
+    @title = title
+    @artist = artist
+    @genre = genre
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
