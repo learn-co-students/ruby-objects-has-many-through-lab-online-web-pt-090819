8bf1def27509d76b1712943107f3d9aa27c10fc1
Chaya Deaver <chayadeaver@gmail.com>
2019-09-29 18:23:29 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,34 @@
+require 'pry'
+
+class Artist
+    attr_accessor :name 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+
+    def self.all
+        @@all
+    end
+
+    def new_song(name, genre)
+        Song.new(name, genre, self)
+
+    end
+
+    def songs
+        binding.pry
+        Song.all.select{|song| song.artist == self}
+
+    end
+
+    def genres
+        songs.collect{|song| song.genre}
+    end
+
+
+
+end
